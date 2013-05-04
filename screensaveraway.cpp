498fc29bddebfeb3d5a4bf824bad14c901f025ce
Martin Klapetek <martin.klapetek@gmail.com>
2013-05-04 13:13:36 +0200

    Add missing files

---
+++ screensaveraway.cpp
@@ -0,0 +1,91 @@
+/*
+    Set away - option when screen saver activated-class
+    Copyright (C) 2013  Lucas Betschart <lucasbetschart@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "screensaveraway.h"
+
+#include <QDBusConnectionInterface>
+#include <QDBusInterface>
+
+#include <TelepathyQt/AccountManager>
+#include <TelepathyQt/AccountSet>
+#include <KDebug>
+#include <KConfig>
+#include <KConfigGroup>
+#include <KTp/global-presence.h>
+
+ScreenSaverAway::ScreenSaverAway(KTp::GlobalPresence *globalPresence, QObject *parent)
+    : TelepathyKDEDModulePlugin(globalPresence, parent)
+{
+    readConfig();
+
+  //watch for screen locked
+  QDBusConnection::sessionBus().connect(QString(),
+				      QLatin1String("/ScreenSaver"),
+				      QLatin1String("org.freedesktop.ScreenSaver"),
+				      QLatin1String("ActiveChanged"),
+				      this,
+				      SLOT(onActiveChanged(bool)) );
+}
+
+ScreenSaverAway::~ScreenSaverAway()
+{
+}
+
+QString ScreenSaverAway::pluginName() const
+{
+    return QString::fromLatin1("screen-saver-away");
+}
+
+void ScreenSaverAway::onActiveChanged(bool newState)
+{
+    if (!isEnabled()) {
+      return;
+    }
+
+    if (newState) {
+      setRequestedPresence(Tp::Presence::away(m_screenSaverAwayMessage));
+      setActive(true);
+    } else {
+      kDebug();
+      setActive(false);
+    }
+}
+
+void ScreenSaverAway::readConfig()
+{
+    KSharedConfigPtr config = KSharedConfig::openConfig(QLatin1String("ktelepathyrc"));
+    config.data()->reparseConfiguration();
+
+    KConfigGroup kdedConfig = config->group("KDED");
+
+    bool screenSaverAwayEnabled = kdedConfig.readEntry("screenSaverAwayEnabled", true);
+
+    m_screenSaverAwayMessage = kdedConfig.readEntry(QLatin1String("screenSaverAwayMessage"), QString());
+
+    if (screenSaverAwayEnabled) {
+        setEnabled(true);
+    } else {
+        setEnabled(false);
+    }
+}
+
+void ScreenSaverAway::onSettingsChanged()
+{
+    readConfig();
+}
---
+++ screensaveraway.h
@@ -0,0 +1,54 @@
+/*
+    Set away - option when screen saver activated-class
+    Copyright (C) 2013  Lucas Betschart <lucasbetschart@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef SCREENSAVERAWAY_H
+#define SCREENSAVERAWAY_H
+
+#include "telepathy-kded-module-plugin.h"
+
+#include <TelepathyQt/Presence>
+#include <TelepathyQt/AccountManager>
+
+namespace KTp {
+class GlobalPresence;
+}
+
+class ScreenSaverAway : public TelepathyKDEDModulePlugin
+{
+    Q_OBJECT
+
+public:
+    explicit ScreenSaverAway(KTp::GlobalPresence *globalPresence, QObject *parent = 0);
+    ~ScreenSaverAway();
+
+    void readConfig();
+    QString pluginName() const;
+
+public Q_SLOTS:
+    void onSettingsChanged();
+
+private Q_SLOTS:
+    void onActiveChanged(bool newState);
+
+private:
+    QString m_screenSaverAwayMessage;
+};
+
+#endif // SCREENSAVERAWAY_H
