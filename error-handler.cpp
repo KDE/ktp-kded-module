73e3307a06eb0c8dadd97481cf31584092154bea
David Edmundson <kde@davidedmundson.co.uk>
2013-06-01 13:35:48 +0100

    Don't supress requested disconnections only to add the error anyway

--- error-handler.cpp
+++ error-handler.cpp
@@ -179,9 +179,7 @@ void ErrorHandler::onConnectionStatusChanged(const Tp::ConnectionStatus status)
         //if this is the first error for this account, store the details of the error to show
         if (account->connectionStatusReason() == Tp::ConnectionStatusReasonRequested) {
             m_errorMap.remove(account);
-        }
-
-        if (!m_errorMap.contains(account)) {
+        } else if (!m_errorMap.contains(account)) {
             m_errorMap.insert(account, ConnectionError(account->connectionStatusReason(), account->connectionError(), account->connectionErrorDetails()));
             QTimer::singleShot(30 * 1000, this, SLOT(showErrorNotification())); //a timer is kept per account because we want to show 30 seconds after the first still valid error.
         }
